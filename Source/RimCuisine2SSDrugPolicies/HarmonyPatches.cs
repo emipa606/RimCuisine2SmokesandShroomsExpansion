03cbff2edbe46326fe7b5aff65db32ff5db8f882
Mlie <emipa606@users.noreply.github.com>
2020-04-21 21:53:08 +0200

    First publish

---
+++ Source/RimCuisine2SSDrugPolicies/HarmonyPatches.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace RimCuisineSSDrugPolicies
+{
+	// Token: 0x02000002 RID: 2
+	[StaticConstructorOnStartup]
+	public static class HarmonyPatches
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		static HarmonyPatches()
+		{
+			var harmonyInstance = new Harmony("mehni.rimworld.rimCuisine.main");
+			harmonyInstance.Patch(AccessTools.Method(typeof(Scenario), "PostGameStart", null, null), null, new HarmonyMethod(typeof(HarmonyPatches), "GenerateStartingDrugPolicies_PostFix", null), null);
+		}
+
+		// Token: 0x06000002 RID: 2 RVA: 0x000020A0 File Offset: 0x000002A0
+		private static void GenerateStartingDrugPolicies_PostFix()
+		{
+			foreach (DrugPolicy drugPolicy in Current.Game.drugPolicyDatabase.AllPolicies)
+			{
+				bool flag = drugPolicy.label == Translator.Translate("SocialDrugs");
+				if (flag)
+				{
+					drugPolicy[RCSSDefOf.RC2_Zope].allowedForJoy = true;
+					drugPolicy[RCSSDefOf.RC2_Cigar].allowedForJoy = true;
+					drugPolicy[RCSSDefOf.RC2_Cigarette].allowedForJoy = true;
+				}
+			}
+		}
+	}
+}
---
+++ Source/RimCuisine2SSDrugPolicies/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyTitle("Mehni RimWorld Mod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mehni RimWorld Mod RimCuisineDrugPolicies")]
+[assembly: AssemblyCopyright("Copyright © Mehni 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("077b95eb-6e4a-44d1-a84f-d492ae1813e9")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
