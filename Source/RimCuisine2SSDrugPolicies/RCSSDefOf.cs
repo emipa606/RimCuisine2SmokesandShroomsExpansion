a70005d7f798601fde572954a92b0186411937d6
emipa606 <emil.offermo@litet.net>
2021-07-21 19:52:42 +0200

    Updated for 1.3

--- Source/RimCuisine2SSDrugPolicies/RCSSDefOf.cs
+++ Source/RimCuisine2SSDrugPolicies/RCSSDefOf.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using RimWorld;
+﻿using RimWorld;
 using Verse;
 
 namespace RimCuisineSSDrugPolicies
 {
-	// Token: 0x02000003 RID: 3
-	[DefOf]
-	public static class RCSSDefOf
-	{
-		// Token: 0x04000001 RID: 1
-		public static ThingDef RC2_Zope;
+    // Token: 0x02000003 RID: 3
+    [DefOf]
+    public static class RCSSDefOf
+    {
+        // Token: 0x04000001 RID: 1
+        public static ThingDef RC2_Zope;
 
-		// Token: 0x04000002 RID: 2
-		public static ThingDef RC2_Cigar;
+        // Token: 0x04000002 RID: 2
+        public static ThingDef RC2_Cigar;
 
-		// Token: 0x04000003 RID: 3
-		public static ThingDef RC2_Cigarette;
-	}
+        // Token: 0x04000003 RID: 3
+        public static ThingDef RC2_Cigarette;
+    }
 }
